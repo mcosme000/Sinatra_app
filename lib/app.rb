4ce9fcbfdeaadcdea6301819c70111a5dc91dec9
Maria Cosmen <mcosme00@gmail.com>
2022-11-15 11:19:56 +0900

    Display 5 random artists on index and display full list of artists from DB on artists page

--- lib/app.rb
+++ lib/app.rb
@@ -1,19 +1,27 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "pry-byebug"
-require "better_errors"
-require "sqlite3"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pry-byebug'
+require 'better_errors'
+require 'sqlite3'
 
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path(__dir__)
 end
 
-DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), "db/jukebox.sqlite"))
+DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'))
 
-get "/" do
-  "Hello from my Sinatra app!"
+get '/' do
+  # "Hello from my Sinatra app!"
   # TODO: Gather all artists to be displayed on home page
   # @artists = DB.execute("SELECT * FROM artists")
   # erb :home # Will render views/home.erb file (embedded in layout.erb)
+  @artists = DB.execute('SELECT * FROM artists').sample(5)
+  erb :index
+end
+
+get '/artists/' do
+  # TODO: Gather all artists to be displayed on home page
+  @artists = DB.execute('SELECT name FROM artists').flatten
+  erb :artists_list
 end
