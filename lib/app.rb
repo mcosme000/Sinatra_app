8bf2cf78e255e3f8f2fdbb61b7effdd2d81d12a2
Maria Cosmen <mcosme00@gmail.com>
2022-11-15 17:34:44 +0900

    Display album list and track list from DB

--- lib/app.rb
+++ lib/app.rb
@@ -12,16 +12,41 @@ end
 DB = SQLite3::Database.new(File.join(File.dirname(__FILE__), 'db/jukebox.sqlite'))
 
 get '/' do
-  # "Hello from my Sinatra app!"
-  # TODO: Gather all artists to be displayed on home page
-  # @artists = DB.execute("SELECT * FROM artists")
-  # erb :home # Will render views/home.erb file (embedded in layout.erb)
-  @artists = DB.execute('SELECT * FROM artists').sample(5)
+  # @artists = DB.execute('SELECT artists.name, albums.title FROM artists
+  #   JOIN albums ON artists.id = albums.artist_id
+  #   WHERE albums.artist_id IN (SELECT albums.artist_id FROM albums
+  #                             GROUP BY albums.artist_id)')
+  @artists = DB.execute('SELECT name FROM artists').sample(5).flatten
   erb :index
 end
 
-get '/artists/' do
+get '/artists_list/' do
   # TODO: Gather all artists to be displayed on home page
-  @artists = DB.execute('SELECT name FROM artists').flatten
+  @artists = DB.execute('SELECT * FROM artists')
   erb :artists_list
 end
+
+get '/artists/:id' do
+  id = params[:id]
+  @artist = DB.execute('SELECT name FROM artists WHERE id = ?', id).flatten.first
+  @albums = DB.execute('SELECT albums.title, albums.id FROM albums
+    JOIN artists ON artists.id = albums.artist_id
+    WHERE artists.id = ?', id)
+  erb :artists
+end
+
+get '/albums/:id' do
+  id = params[:id]
+  @artist = DB.execute('SELECT artists.id, artists.name FROM artists
+    JOIN albums ON artists.id = albums.artist_id
+    WHERE albums.id = ?', id).flatten
+  @album = DB.execute('SELECT title FROM albums WHERE id = ?', id).flatten.first
+  @tracks = DB.execute('SElECT tracks.name FROM tracks
+    JOIN albums ON albums.id = tracks.album_id
+    WHERE albums.id = ?', id).flatten
+  erb :albums
+end
+
+# get '/tracks/:id' do
+#   id = params[:id]
+# end
